1feb5f0875b24012296cea9236361fd7ab1a5395
VinceOmega <vince.omega@gmail.com>
2015-08-27 14:38:30 -0400

    Syntax errors fix

--- public/application/views/mcb/order_page.php
+++ public/application/views/mcb/order_page.php
@@ -64,9 +64,9 @@
 					Choose Quantity<br>
 
 					<?php foreach($orderresults[0] as $orders):?>
-					<a href="#" onclick="javascript:;" class="reduce">-</a> <input type="text" name="orders_basket[<? echo $orderresults[0]s->id; ?>]" class="number-field" value="<?php echo $orderresults[0]s->qty; ?>"><a href="#" onclick="javascript:;" class="plus">+</a><br>
+					<a href="#" onclick="javascript:;" class="reduce">-</a> <input type="text" name="orders_basket[<? echo $orders->id; ?>]" class="number-field" value="<?php echo $orders->qty; ?>"><a href="#" onclick="javascript:;" class="plus">+</a><br>
 					<a href="#" class="update">Update</a>
-					<input type="hidden" name="rate" class="item-rate" value="<?= isset($orderresults[0]->rate)?>">
+					<input type="hidden" name="rate" class="item-rate" value="<?= isset($orders->rate)?>">
 					<?php endforeach; ?>
 					</span>
 			</div>
